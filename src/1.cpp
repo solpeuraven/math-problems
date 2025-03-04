d7bf488fc7b680d1668d7b5a855c0faad3fe7563
solpeuraven <solpeuraven@gmx.com>
2025-03-04 20:47:04 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << std::endl;
+    return 0;
+}
